453193fc7a32cc53b3b3a5811e47c68b2df7c3d0
sime-81 <yapock123@gmail.com>
2023-12-30 13:38:36 +0100

    first part crypto-watch header 1

---
+++ src/components/HeaderInfos.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import PercentChange from "./PercentChange";
+
+const HeaderInfos = () => {
+  const [headerData, setHeaderData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.coingecko.com/api/v3/global")
+      .then((res) => setHeaderData(res.data.data));
+  }, []);
+
+  return (
+    <div className="header-container">
+      <ul className="title">
+        <li>
+          <h1>
+            <img src="./assets/logo.png" alt="logo" /> Watch Tower
+          </h1>
+        </li>
+        <li>
+          Crypto-monnaies :{" "}
+          {headerData.active_cryptocurrencies &&
+            headerData.active_cryptocurrencies.toLocaleString()}
+        </li>
+        <li>Machés : {headerData.markets && headerData.markets}</li>
+      </ul>
+      <ul className="infos-mkt">
+        <li className="global-mkt">
+          Global Market Cap :
+          <PercentChange
+            percent={headerData.market_cap_change_percentage_24h_usd}
+          />
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default HeaderInfos;
